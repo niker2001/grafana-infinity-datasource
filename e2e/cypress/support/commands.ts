0b539af057fa0d982e9dd8ad434972e1c2846e69
Sriram <yesoreyeram@gmail.com>
2021-07-03 09:11:45 +0100

    initial e2e code

---
+++ e2e/cypress/support/commands.ts
@@ -0,0 +1,23 @@
+interface CompareScreenshotsConfig {
+  name: string;
+  threshold?: number;
+}
+
+Cypress.Commands.add('logToConsole', (message: string, optional?: any) => {
+  cy.task('log', { message: '(' + new Date().toISOString() + ') ' + message, optional });
+});
+
+Cypress.Commands.add('compareScreenshots', (config: CompareScreenshotsConfig | string) => {
+  cy.task('compareScreenshots', {
+    config,
+    screenshotsFolder: Cypress.config('screenshotsFolder'),
+    specName: Cypress.spec.name,
+  });
+});
+
+Cypress.Commands.add('readProvisions', (filePaths: string[]) => {
+  cy.task('readProvisions', {
+    CWD: Cypress.env('CWD'),
+    filePaths,
+  });
+});
